d35e5dc02fed3cf296e3b22eaf49d3643c71cd00
cudan <cud.andrei@gmail.com>
2022-09-17 22:19:38 +0300

    Revert "Ready to PR."
    This reverts commit 730772bf16e2e22712bf5cdc7077c3fb34b226c2.

--- ProductsWebAPI/ProductsWebAPI.Tests/AccountControllerTests.cs
+++ ProductsWebAPI/ProductsWebAPI.Tests/AccountControllerTests.cs
@@ -26,10 +26,9 @@ namespace ProductsWebAPI.Tests
         {
             Mock<IConfiguration> mockConfig = new Mock<IConfiguration>();
 
-            mockConfig.SetupGet(s => s[It.Is<string>(str => str == "Jwt:Key")]).Returns("Yh2k7QSu4l8CZg5p6X3Pna9L0Miy4D3Bvt0JVr87UcOj69Kqw5R2Nmf4FWs03Hdx");
-            mockConfig.SetupGet(s => s[It.Is<string>(str => str == "Jwt:Issuer")]).Returns("JWTAuthenticationServer");
-            mockConfig.SetupGet(s => s[It.Is<string>(str => str == "Jwt:Audience")]).Returns("JWTServicePostmanClient");
-            mockConfig.SetupGet(s => s[It.Is<string>(str => str == "Jwt:Subject")]).Returns("JWTServiceAccessToken");
+            mockConfig
+                .Setup(c => c.GenerateJwtToken(It.IsAny<User>()))
+                .Returns("123");
 
             Mock<IAccountService<User, RegisterModel>> mockAccountService = new Mock<IAccountService<User, RegisterModel>>();
             
@@ -39,17 +38,7 @@ namespace ProductsWebAPI.Tests
                 {
                     Login = "user", Password = "123".GetHashCode(), Role = Role.User
                 });
-
-            mockAccountService
-                .Setup(s => s.GetAll())
-                .Returns(new List<User>()
-                {
-                    new User()
-                    {
-                    Login = "user", Password = "123".GetHashCode(), Role = Role.User
-                    }
-                });
-
+            
             _accountController = new AccountController(mockAccountService.Object, mockConfig.Object);
         }
 
@@ -73,54 +62,6 @@ namespace ProductsWebAPI.Tests
             // Assert
 
             Assert.IsTrue(actualResult is OkObjectResult);
-        }
-
-        [TestMethod]
-        public void Register_NotValidModel_BadStatus()
-        {
-            // Arrange
-
-            var registerModel = new RegisterModel() { Login = "user", Password = "123", PasswordConfirm = "123" };
-
-            // Act
-
-            var actualResult = _accountController.Register(registerModel);
-
-            // Assert
-
-            Assert.IsTrue(actualResult is BadRequestObjectResult);
-        }
-
-        [TestMethod]
-        public void Login_ValidLogin_OkStatus()
-        {
-            // Arrange
-
-            var loginModel = new LoginModel() { Login = "user", Password = "123" };
-
-            // Act
-
-            var actualResult = _accountController.Login(loginModel);
-
-            // Assert
-
-            Assert.IsTrue(actualResult is OkObjectResult);
-        }
-
-        [TestMethod]
-        public void Login_NotValidLogin_BadStatus()
-        {
-            // Arrange
-
-            var loginModel = new LoginModel() { Login = "user2", Password = "123" };
-
-            // Act
-
-            var actualResult = _accountController.Login(loginModel);
-
-            // Assert
-
-            Assert.IsTrue(actualResult is BadRequestObjectResult);
         }
     }
 }
